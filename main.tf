7b3acae2ebaff456b0b9178bb1b27354a465269d
Alex Chan <a.chan@wellcome.org>
2021-07-09 07:45:39 +0100

    Add an implementation of the module

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-// TODO: Do me
+resource "aws_secretsmanager_secret" "secret" {
+  for_each = var.key_value_map
+
+  name = each.key
+}
+
+resource "aws_secretsmanager_secret_version" "secret" {
+  for_each = var.key_value_map
+
+  secret_id     = aws_secretsmanager_secret.secret[each.key].id
+  secret_string = each.value
+}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "example_output" {
-  value       = var.example_variable
-  description = "A simple description of the output and how it's used. Please include in all outputs."
-}
