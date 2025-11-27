1deb06927128addb0afb8a892d73be83b60db3c1
Štěpán Brychta <stepan.brychta@me.com>
2025-11-27 14:51:20 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,10 @@ output "arns" {
     aws_secretsmanager_secret_version.secret[key].arn
   ]
 }
+
+output "versions" {
+  value = [
+    for key, _ in var.key_value_map :
+    aws_secretsmanager_secret_version.secret[key].version_id
+  ]
+}
